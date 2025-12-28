100a83953dbe97ab91b0f9791f874a39fed54c49
JaiK23 <jaiksailor7@gmail.com>
2025-12-29 00:19:01 +0530

    changes made

--- ExpenseTrackerWebsite/client/expense-tracker/src/api.js
+++ ExpenseTrackerWebsite/client/expense-tracker/src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-
-const BASE_URL = "http://localhost:8000/api";
+// const BASE_URL = "http://localhost:8000/api";
+const BASE_URL =
+  import.meta.env.VITE_API_BASE_URL?.replace(/\/$/, "") ||
+  "http://localhost:8000/api";
 let authToken =
   typeof localStorage !== "undefined"
     ? localStorage.getItem("authToken")
